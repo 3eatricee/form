894ffd0b40fecc00faccc5a5f9ffc8f36f65223c
Beatrice Ambos <beatriceambos@MacBook-Pro-von-Beatrice.local>
2025-04-03 16:10:26 +0200

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { z } from 'zod';
+	import 'tailwindcss';
+	import { superForm } from 'sveltekit-superforms';
+  
+	let email: string = '';
+	let password: string = '';
+
+	const schema = z.object({
+	  email: z.string().email('unvalid mail adress'),
+	  password: z.string().min(8, 'password needs min a length from 8').regex(/[!@#$%^&*(),.?":{}|<>]/, "password needs special characters"),
+	});
+  
+	// const { form, errors } = superForm(
+	// 	{ schema, }
+	// )
+
+	const handleSubmit = (event: Event) => {
+	  event.preventDefault();
+	  const result = schema.safeParse({ email, password });
+	  console.log(result)
+	};
+  </script>
+  
+  <form on:submit={handleSubmit}>
+	<label>
+	  Email
+	  <input name="email" type="email" bind:value={email}>
+	</label>
+	<label>
+		Password
+		<input name="password" bind:value="{password}">
+	</label>
+	<button type="submit">Login</button>
+  </form>
+  
